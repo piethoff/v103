c93d25a88e76dbb031ef5ad077bff37313b0660f
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2017-12-08 14:02:43 +0100

    added curvefit for einseit

--- plot.py
+++ plot.py
@@ -1,27 +1,73 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+from scipy.optimize import curve_fit
+import scipy.constants.constants as const
+from uncertainties import ufloat
+from uncertainties import unumpy
+
 
 mpl.use('pgf')
 mpl.rcParams.update({
     'pgf.preamble': r'\usepackage{siunitx}',
 })
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+data = np.genfromtxt('content/rund_einseit.txt', unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+x = 0.5 * (data[0] /100)**2 - (data[0] / 300)**3
+y= (data[2] -data[1]) /1000
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+plt.xlabel(r'$g(x)/\si{\cubic\meter}$')
+plt.ylabel(r'$D(x)/\si{\meter}$')
+plt.grid(True, which='both')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+
+# Fitvorschrift
+def f(x, A, B):
+    return A*x + B      #jeweilige Fitfunktion auswaehlen:
+
+params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+for i in range(0, len(uparams)):
+    print(chr(ord('A') + i), "=" , uparams[i])
+print()
+
+plt.plot(x, f(x, *params), "xkcd:orange", label=r'Regression' )
+plt.plot(x, y, ".", color="xkcd:blue", label="Messwerte")
+
+
+
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/r_einseit.pdf')
+plt.clf()
+
+data = np.genfromtxt('content/eckig_einseit.txt', unpack=True)
+
+x = 0.5 * (data[0] /100)**2 - (data[0] / 300)**3
+y= (data[2] -data[1]) /1000
+
+plt.xlabel(r'$g(x)/\si{\cubic\meter}$')
+plt.ylabel(r'$D(x)/\si{\meter}$')
+plt.grid(True, which='both')
+
+
+# Fitvorschrift
+def f(x, A, B):
+    return A*x + B      #jeweilige Fitfunktion auswaehlen:
+
+params, covar = curve_fit(f, x, y)            #eigene Messwerte hier uebergeben
+uparams = unumpy.uarray(params, np.sqrt(np.diag(covar)))
+for i in range(0, len(uparams)):
+    print(chr(ord('A') + i), "=" , uparams[i])
+print()
+
+plt.plot(x, f(x, *params), "xkcd:orange", label=r'Regression' )
+plt.plot(x, y, ".", color="xkcd:blue", label="Messwerte")
+
+
+
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/e_einseit.pdf')
+plt.clf()
